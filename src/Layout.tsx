e9737533a780312fb62fde5ce0d9a7b421f1a983
Seu Nome <vitoriaglordeiro@gmail.com>
2025-08-16 07:32:43 -0300

    declarando modulos

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,8 +1,7 @@
-    // src/components/Layout/index.tsx
+    
     import Header from "./components/Header";
     import Footer from "./components/Footer";
     import { Outlet } from "react-router-dom";
-    import Perfil from " ../Perfil"
 
     const Layout = () => {
     return (
